1a5568de8756ee9370321db74ad02b33b5759c95
ROOT <1v1expert@gmail.com>
2018-07-19 01:47:57 +0300

    Fix and make app for win x32 x64

---
+++ form2.spec
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['form2.py'],
+             pathex=['C:/Python34/Lib/site-packages/PyQt5', 'C:\\Users\\ROOT\\Desktop\\CTable\\Circle_Table'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='form2',
+          debug=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name='form2')
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import os, sys
+import py2exe
+from glob import glob
+import PyQt5
+
+NAME="Proga"
+
+qt_platform_plugins = [("platforms", glob(PyQt5.__path__[0] + r'\plugins\platforms\*.*'))]
+data_files.extend(qt_platform_plugins)
+msvc_dlls = [('.', glob(r'C:\Windows\System32\msvc?100.dll'))]
+data_files.extend(msvc_dlls)
+# print(data_files)
+
+sys.argv.append('py2exe')
+
+setup(
+	data_files=data_files,
+	windows=[
+		{
+			"script": "form2.py",
+		}
+	],
+	# zipfile=None,
+	options={
+		"py2exe": {
+			"includes":["sip", "atexit",],  # даже если мы нигде не используем atexit его надо добавить, sip указываем явно - сборщики часто про него забывают 
+			# "packages": ['PyQt5'],
+			"compressed": True,
+			"dist_dir": "dist/" + NAME,
+			# "bundle_files": 0, # не сработало ( с этой опцией на выходе прога не может найти msvc*.dll 
+			# "zipfile": None, # тоже лишнее
+			"optimize": 2,
+		}
+	}
+)
